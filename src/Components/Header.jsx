63d2e754c6d9c14ff79a73ffe5401aac762cc5b2
atiii <aty.sokhansanj@gmail.com>
2020-06-28 13:34:30 -0700

    6/28/2020 v02

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,14 +4,18 @@ import React, { Component } from 'react'
 //import css
 import './../css/reset.css'
 import './../css/Header.css';
-import './../css/formlogin.css'
+import './../css/formlogin.css';
 import logo2 from './../images/logo2.png';
 
 //import boostrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap'
 import { Modal } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card';
 
+//import routes
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     // function FormExample() {
@@ -26,88 +30,97 @@ export default class Header extends Component {
 
     //     setValidated(true);
     // };
+
+    state = {
+        show: false
+    }
+
+    handleShow = () =>{ this.setShow(true)};
     render() {
         return (
             <>
                 <div className="container-fluid">
                     <div className="navbar navbar-expand-lg">
                         <div className="nav-right">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    {/* <Button className=""> */}
-                                    <a className="nav-link " id="login" to="/login" data-target="#loginModal" data-toggle="modal">LOG IN</a>
-                                    {/* </Button> */}
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Button className=""> */}
-                                    <a className="nav-link" id="signin" to="/signin">SIGN UP</a>
-                                    {/* </Button> */}
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Button type="button"> */}
-                                    <a className="nav-link" to="more">More</a>
-                                    {/* </Button> */}
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Button className=""> */}
-                                    <a className="nav-link" to="about">About</a>
-                                    {/* </Button> */}
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Button className=""> */}
-                                    <a className="nav-link" to="news">News</a>
-                                    {/* </Button> */}
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Button className=""> */}
-                                    <a className="nav-link" to="studi">Studi</a>
-                                    {/* </Button> */}
-                                </li>
-                            </ul>
+                            <BrowserRouter>
+                                <Route>
+                                    <ul className="navbar-nav">
+                                        <li className="nav-item">
+                                            {/* <Button className=""> */}
+                                            <Link className="nav-link " id="login" to="/login" onClick={this.handleShow}>LOG IN</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                        <li className="nav-item">
+                                            {/* <Button className=""> */}
+                                            <Link className="nav-link" id="signin" to="/signin">SIGN UP</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                        <li className="nav-item">
+                                            {/* <Button type="button"> */}
+                                            <Link className="nav-link" to="more">More</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                        <li className="nav-item">
+                                            {/* <Button className=""> */}
+                                            <Link className="nav-link" to="about">About</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                        <li className="nav-item">
+                                            {/* <Button className=""> */}
+                                            <Link className="nav-link" to="news">News</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                        <li className="nav-item">
+                                            {/* <Button className=""> */}
+                                            <Link className="nav-link" to="studi">Studi</Link>
+                                            {/* </Button> */}
+                                        </li>
+                                    </ul>
+                                </Route>
+                            </BrowserRouter>
+
                         </div>
                         <div className="nav-left">
                             <div className="empty-content"></div>
                             <h4 className="UFOGame">UFO<span>Game</span></h4>
-                            <img className="logo" src={logo2} />
+                            <img className="logo" src={logo2} alt="" />
                         </div>
                     </div>
 
-                    <Modal className="modal fade" id="loginModal">
+                    <Modal className="modal fade" show={this.state.show} id="loginModal">
                         <Modal.Dialog className="modal-dialogs modal-dialog-centered modal-sm">
                             <div className="form-register modal-content mt-3">
                                 <Modal.Body className="modal-bodys d-block">
-                                    <div className="card">
-                                        <div className="card-body px-0">
-                                            <div className="card-title text-center">
+                                    <Card>
+                                        <Card.Body className="px-0">
+                                            <Card.Title className="text-center">
                                                 <h2>Welcome <span>back</span></h2>
-                                                <p className="text-white">Do not have account ? <a href="#">Sign up</a></p>
-                                            </div>
+                                                <p className="text-white">Do not have account ? <Link to="#">Sign up</Link></p>
+                                            </Card.Title>
                                             <Form action="#" className="form-login py-4" >
                                                 <div className="form-fields mb-4">
                                                     <label>
                                                         : User name
                                                         <input type="text" className="form-control mb-2" id="username"
-                                                        placeholder="User" required></input>
-                                                        </label>
-                                          
+                                                            placeholder="User" required></input>
+                                                    </label>
+
                                                 </div>
                                                 <div className="form-fields mb-4">
                                                     <label>
                                                         : Password
                                                         <input type="password" className="form-control mb-2" id="password"
-                                                        placeholder="Password" required></input>
+                                                            placeholder="Password" required></input>
                                                     </label>
                                                 </div>
 
                                                 <div className="mt-5 form-group">
-                                                    <button className="btn btn-info btn-lg btn-block" id="submit" type="submit">LOG
-                                                                 IN</button>
+                                                    <Button className="btn btn-info btn-lg btn-block" id="submit" type="submit">LOG
+                                                                 IN</Button>
                                                 </div>
-
-
                                             </Form>
-                                        </div>
-                                    </div>
+                                        </Card.Body>
+                                    </Card>
                                 </Modal.Body>
 
                             </div>
