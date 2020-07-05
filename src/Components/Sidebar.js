afb77e8175ecc19ca56ac9f94212d7c4661c4374
atiii <aty.sokhansanj@gmail.com>
2020-07-04 17:00:15 -0700

    7/4/2020 v03

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,36 +1,35 @@
-import React, { Component }from 'react';
+import React, { Component } from "react";
 
 //import css
-import './../css/reset.css';
-import './../css/sidebar.css'
-
+import "./../css/reset.css";
+import "./../css/sidebar.css";
 
 //import routes
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-class Sidebar extends Component{
-    render(){
-        return(
-            <>
-            <div className="sidebar-menu">
-                <ul className="list-menu">
-                    <li className="list-item">Home</li>
-                    <li className="list-item">My account</li>
-                    <li className="list-item">Console</li>
-                    <li className="list-item">Games</li>
-                    <li className="list-item">Liga</li>
-                    <li className="list-item">Tourney</li>
-                    <li className="list-item">TvLive</li>
-                    <li className="list-item">Leader boards</li>
-                    <li className="list-item">Studi</li>
-                    <li className="list-item">News</li>
-                    <li className="list-item">Help center</li>
-                    <li className="list-item">Chat</li>
-                </ul>
-            </div>
-            </>
-        )
-    }
+class Sidebar extends Component {
+  render() {
+    return (
+      <>
+        <div className="sidebar-menu">
+          <ul className="list-menu">
+            <li className="list-item"><NavLink>Home</NavLink></li>
+            <li className="list-item"><NavLink>My account</NavLink></li>
+            <li className="list-item"><NavLink>Console</NavLink></li>
+            <li className="list-item"><NavLink>Games</NavLink></li>
+            <li className="list-item"><NavLink>Liga</NavLink></li>
+            <li className="list-item"><NavLink>Tourney</NavLink></li>
+            <li className="list-item"><NavLink>TvLive</NavLink></li>
+            <li className="list-item"><NavLink>Leader boards</NavLink></li>
+            <li className="list-item"><NavLink>Studi</NavLink></li>
+            <li className="list-item"><NavLink>News</NavLink></li>
+            <li className="list-item"><NavLink>Help center</NavLink></li>
+            <li className="list-item"><NavLink>Chat</NavLink></li>
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
