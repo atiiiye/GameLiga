2d643543d26e734687e1761f0bd3974572252bf4
atiii <aty.sokhansanj@gmail.com>
2020-07-22 21:05:29 +0430

    1/05/99 v04

--- src/Components/LeaderBoards.js
+++ src/Components/LeaderBoards.js
@@ -5,7 +5,7 @@ import "./../css/reset.css";
 import "./../css/LeaderBoards.css";
 
 //import bootstrap
-import { Card } from "react-bootstrap";
+import { Card ,Form } from "react-bootstrap";
 
 //import images
 import Football from './../images/football.png'
@@ -28,8 +28,8 @@ class LeaderBoards extends Component {
                                     <div className="section-left">
                                         <div className="own-logo">
                                             <img alt="" src={UserLogo} className="user-team-logo" />
-                                            <div className="result-win">
-                                                <span className=""></span>
+                                            <div className="difference">
+                                                <Form.Control type="number" className="difference-number" value="0"></Form.Control>
                                             </div>
                                         </div>
                                         <div className="football">
@@ -37,8 +37,8 @@ class LeaderBoards extends Component {
                                         </div>
                                         <div className="other-logo">
                                             <img alt="" src={UserLogo} className="user-team-logo" />
-                                            <div className="result-lose">
-                                                <span className=""></span>
+                                            <div className="difference">
+                                                <Form.Control type="number" className="difference-number" value="0"></Form.Control>
                                             </div>
                                         </div>
                                     </div>
