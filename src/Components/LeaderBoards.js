9d96e79acdd9228c74b9b79483c57d9b448c889d
atiii <aty.sokhansanj@gmail.com>
2020-07-27 20:32:10 +0430

    06/05/99 v03

--- src/Components/LeaderBoards.js
+++ src/Components/LeaderBoards.js
@@ -20,6 +20,14 @@ class LeaderBoards extends Component {
     state = {
         ownValue: "0",
         otherValue: "0",
+        idCard: [
+            {
+                id:1
+            },
+            {
+                id:2
+            }
+        ]
     }
 
     handleChange = (e) => {
@@ -27,13 +35,19 @@ class LeaderBoards extends Component {
     }
 
     createGame = () => {
-        let tabContent = document.querySelector('div.tab-content-parent')
+        let tabContent = document.querySelector('div.card#tab-content')
+        console.log(tabContent);
+
         let card = document.createElement('div')
-        // let newGame
         card.classList.add('card')
-        card.setAttribute('id','tab-content')
-        card.appendChild(tabContent)
+        card.setAttribute('id', 'tab-content')
         console.log(card);
+
+        card.appendChild(tabContent)
+        // tabContent.appendChild('')
+        tabContent.after('card')
+        // card.after(tabs)
+        // console.log(card);
 
         // tabContent.after('div#tab-content')
 
@@ -49,102 +63,101 @@ class LeaderBoards extends Component {
                             <div className="tourny tab" data-content="tourny">Tourny</div>
                             <div className="liga tab" data-content="liga">Liga</div>
                         </div>
-                        <Card id="tab-content">
-                            <div className="tab-content-parent">
-                                <div className="tab-content-left">
-                                    <div className="section-left">
-                                        <div className="own-logo">
-                                            <img alt="" src={UserLogo} className="user-team-logo" />
-                                            <div className="difference">
-                                                <Form.Control
-                                                    type="number"
-                                                    name="ownValue"
-                                                    className="difference-number"
-                                                    value={this.state.ownValue}
-                                                    onChange={this.handleChange.bind(this)}
-                                                    max={10}
-                                                    min={-10}
-                                                    maxLength="2"
-                                                ></Form.Control>
+                        {this.state.idCard.map(item => (
+                            <Card id="tab-content" key={item.idCard}>
+                                <div className="tab-content-parent">
+                                    <div className="tab-content-left">
+                                        <div className="section-left">
+                                            <div className="own-logo">
+                                                <img alt="" src={UserLogo} className="user-team-logo" />
+                                                <div className="difference">
+                                                    <Form.Control
+                                                        type="number"
+                                                        name="ownValue"
+                                                        className="difference-number"
+                                                        value={this.state.ownValue}
+                                                        onChange={this.handleChange.bind(this)}
+                                                        max={10}
+                                                        min={-10}
+                                                        maxLength="2"
+                                                    ></Form.Control>
+                                                </div>
+                                            </div>
+                                            <div className="football">
+                                                <img alt="" src={Football} className="football-field" />
+                                            </div>
+                                            <div className="other-logo">
+                                                <img alt="" src={UserLogo} className="user-team-logo" />
+                                                <div className="difference">
+                                                    <Form.Control
+                                                        type="number"
+                                                        name="otherValue"
+                                                        className="difference-number"
+                                                        value={this.state.otherValue}
+                                                        onChange={this.handleChange.bind(this)}
+                                                        max={10}
+                                                        min={-10}
+                                                        maxLength="2"
+                                                    ></Form.Control>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="football">
-                                            <img alt="" src={Football} className="football-field" />
-                                        </div>
-                                        <div className="other-logo">
-                                            <img alt="" src={UserLogo} className="user-team-logo" />
-                                            <div className="difference">
-                                                <Form.Control
-                                                    type="number"
-                                                    name="otherValue"
-                                                    className="difference-number"
-                                                    value={this.state.otherValue}
-                                                    onChange={this.handleChange.bind(this)}
-                                                    max={10}
-                                                    min={-10}
-                                                    maxLength="2"
-                                                ></Form.Control>
+                                        <span className="hosted">Hosted by Alex52 / Div2</span>
+                                    </div>
+                                    <div className="tab-content-right">
+                                        <div className="section-right">
+                                            <img src={JoinButton} className="join-button" />
+                                            <div className="options">
+                                                <div className="option">
+                                                    <span className="option-item">Time game :</span>
+                                                    <FormControl
+                                                        className="time num-minute"
+                                                        type="number"
+                                                        name=""
+                                                        max={59}
+                                                        min={2}
+                                                        maxLength="2"
+                                                    ></FormControl>
+                                                    <FormControl
+                                                        className="time num-second"
+                                                        type="number"
+                                                        name=""
+                                                        max={59}
+                                                        min={0}
+                                                        maxLength="2"
+                                                    ></FormControl>
+                                                    <FormControl
+                                                        className="time"
+                                                        type="text"
+                                                        name="type"
+                                                        maxLength="3"
+                                                    ></FormControl>
+                                                </div>
+                                                <div className="option">
+                                                    <span className="option-item">Buy in :</span>
+                                                    <FormControl
+                                                        className=""
+                                                        type="text"
+                                                        name="buy"
+                                                    ></FormControl>
+                                                </div>
+                                                <div className="option">
+                                                    <span className="option-item">Advantage :</span>
+                                                    <FormControl
+                                                        className="advantage"
+                                                        type="number"
+                                                        name="advantage"
+                                                        max={10}
+                                                        min={-10}
+                                                    ></FormControl>
+                                                </div>
+                                                <span className="No">No.T0001</span>
                                             </div>
                                         </div>
                                     </div>
-                                    <span className="hosted">Hosted by Alex52 / Div2</span>
                                 </div>
-                                <div className="tab-content-right">
-                                    <div className="section-right">
-                                        <img src={JoinButton} className="join-button" />
-                                        <div className="options">
-                                            <div className="option">
-                                                <span className="option-item">Time game :</span>
-                                                <FormControl
-                                                    className="time num-minute"
-                                                    type="number"
-                                                    name=""
-                                                    max={59}
-                                                    min={2}
-                                                    maxLength="2"
-                                                ></FormControl>
-                                                <FormControl
-                                                    className="time num-second"
-                                                    type="number"
-                                                    name=""
-                                                    max={59}
-                                                    min={0}
-                                                    maxLength="2"
-                                                ></FormControl>
-                                                <FormControl
-                                                    className="time"
-                                                    type="text"
-                                                    name="type"
-                                                    maxLength="3"
-                                                ></FormControl>
-                                            </div>
-                                            <div className="option">
-                                                <span className="option-item">Buy in :</span>
-                                                <FormControl
-                                                    className=""
-                                                    type="text"
-                                                    name="buy"
-                                                ></FormControl>
-                                            </div>
-                                            <div className="option">
-                                                <span className="option-item">Advantage :</span>
-                                                <FormControl
-                                                    className="advantage"
-                                                    type="number"
-                                                    name="advantage"
-                                                    max={10}
-                                                    min={-10}
-                                                ></FormControl>
-                                            </div>
-                                            <span className="No">No.T0001</span>
-                                        </div>
-
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
+                            </Card>
+                        ))}
 
                     </div>
                     <div className="sidebar-right">
